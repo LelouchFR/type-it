4cb53c498ea5eb078cf30217b4aaa76aa7c5b372
LelouchFR <baptistezahnow@gmail.com>
2023-06-06 23:31:23 +0200

    Patch Release 1.3.0 Just Color Theme fix (again but for a logn time the last one)

--- src/main.ts
+++ src/main.ts
@@ -211,15 +211,15 @@ function getColors(theme: string): ColorThemes {
         'Cosmic Cobalt': { primaryColor: '#3626A7', secondaryColor: '#657ED4' },
         'Nautical Night': { primaryColor: '#001524', secondaryColor: '#15616D' },
         'Cocoa Bean': { primaryColor: '#210F04', secondaryColor: '#452103' },
-        'Botanical Garden': { primaryColor: '#3A606E', secondaryColor: '#607B7D' },
-        'Deep Amethyst': { primaryColor: '#D36135', secondaryColor: '#7FB069' },
-        'Muted Teal': { primaryColor: '#210F04', secondaryColor: '#452103' },
+        'Botanical Garden': { primaryColor: '#3FA34D', secondaryColor: '#5BBA6F' },
+        'Deep Amethyst': { primaryColor: '#190B28', secondaryColor: '#685762' },
+        'Muted Teal': { primaryColor: '#3A606E', secondaryColor: '#607B7D' },
         'Warm Spice': { primaryColor: '#D36135', secondaryColor: '#7FB069' },
         'Heavy Umber': { primaryColor: '#191308', secondaryColor: '#322A26' },
-        'Dreamy Lavender': { primaryColor: '#685762', secondaryColor: '#190B28' },
+        'Dreamy Lavender': { primaryColor: '#5d009f', secondaryColor: '#ae7fd0' },
         'Earthen Haze': { primaryColor: '#272727', secondaryColor: '#D4AA7D' },
     };
-
+    
     return colorThemes;
 }
 
